529a9b5a59108346eae7134e4c1db272648c8471
Arrafi2644 <raffian2644@gmail.com>
2024-10-23 08:37:38 +0600

    Basic setup complete

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='bg-red-400 text-2xl font-bold'>React knowledge cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
